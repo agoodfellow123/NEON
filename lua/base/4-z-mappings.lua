d0fc27aeb20ad64493e6cf343e492b65237a876d
xnrdotone <xtrafun.xanrer@gmail.com>
2024-04-06 01:27:46 +0300

    up

--- lua/base/4-z-mappings.lua
+++ lua/base/4-z-mappings.lua
@@ -3,49 +3,271 @@ local utils = require "base.utils"
 local ui = require "base.utils.ui"
 local maps = require("base.utils").get_mappings_template()
 
+
+org_key = {
+">",
+"{",
+"\"",
+"<",
+":",
+"}",
+"±",
+"@",
+"#",
+"$",
+"^",
+"&",
+"*",
+"(",
+")",
+"_",
+"+",
+"~",
+"|",
+"?",
+".",
+"[",
+"\'",
+",",
+";",
+"]",
+"i",
+"§",
+"-",
+"=",
+"\\",
+"/",
+"`",
+"™",
+"£",
+"¢",
+"∞",
+"¶",
+"•",
+"ª",
+"º",
+"–",
+"≠",
+"œ",
+"´",
+"†",
+"¥",
+"‘",
+"…",
+"«",
+}
+
+tur_key = {
+"Ç",
+"Ğ",
+"İ",
+"Ö",
+"Ş",
+"Ü",
+"é",
+"'",
+"^",
+"+",
+"&",
+"/",
+"(",
+")",
+"=",
+"?",
+"_",
+">",
+";",
+":",
+"ç",
+"ğ",
+"i",
+"ö",
+"ş",
+"ü",
+"ı",
+"\"",
+"*",
+"-",
+",",
+".",
+"<",
+"£",
+"#",
+"$",
+"½",
+"{",
+"[",
+"]",
+"}",
+"\\",
+"|",
+"@",
+"€",
+"₺",
+"¥",
+"~",
+"´",
+"`",
+}
+
+for i in ipairs(tur_key) do
+    maps.n[tur_key[i]] = {org_key[i]}
+    maps.v[tur_key[i]] = {org_key[i]}
+    maps.o[tur_key[i]] = {org_key[i]}
+end
+
+
+
 maps.n["<leader>la"] = { "<cmd>Lspsaga code_action<cr>", desc = "Code action",}
 maps.n["<C-Space>"] = { "<cmd>Lspsaga code_action<cr>", desc = "Code action",}
 maps.n["<leader>lh"] = { "<cmd>Lspsaga hover_doc<cr>", desc = "Hover doc",}
 maps.n["<Tab>"] = { "<cmd>Lspsaga hover_doc<cr>", desc = "Hover doc",}
 maps.n["<leader>lr"] = { "<cmd>Lspsaga rename<cr>", desc = "Rename",}
 
--- maps.n["j"] = {"gj"}
--- maps.n["k"] = {"gk"}
--- maps.n["<Down>"] = {"gj"}
--- maps.n["<Up>"] = {"gk"}
---
--- maps.v["j"] = {"gj"}
--- maps.v["k"] = {"gk"}
--- maps.v["<Down>"] = {"gj"}
--- maps.v["<Up>"] = {"gk"}
---
--- maps.n["0"] = {"g0"}
--- maps.n["-"] = {"g$"}
--- maps.v["0"] = {"g0"}
--- maps.v["-"] = {"g$"}
---
--- maps.n["Q"] = {"gq"}
+maps.n["<Down>"] = {"gj"}
+maps.n["<Up>"] = {"gk"}
 
--- vim.opt.whichwrap = vim.opt.whichwrap + "h"
--- vim.opt.whichwrap = vim.opt.whichwrap + "l"
+maps.v["<Down>"] = {"gj"}
+maps.v["<Up>"] = {"gk"}
 
--- maps.n["ı"] = {"i"}
+maps.n["0"] = {"g0"}
+maps.n["-"] = {"g$"}
+maps.v["0"] = {"g0"}
+maps.v["-"] = {"g$"}
+
+maps.n["Q"] = {"gq"}
+
+vim.opt.whichwrap = vim.opt.whichwrap + "h"
+vim.opt.whichwrap = vim.opt.whichwrap + "l"
+
+-- maps.n["j"] = {"h"}
+-- maps.n["k"] = {"gj"}
+-- maps.n["l"] = {"gk"}
+-- maps.n[";"] = {"l"}
+-- maps.n["'"] = {";"}
+-- maps.n["\\"] = {"'"}
+-- maps.n["h"] = {"\\"}
+--
+-- maps.n["J"] = {"H"}
+-- maps.n["K"] = {"J"}
+-- maps.n["L"] = {"K"}
+-- maps.n[":"] = {"L"}
+-- maps.n["\""] = {":"}
+-- maps.n["|"] = {"\""}
+-- maps.n["H"] = {"|"}
+
 maps.n["j"] = {"h"}
-maps.n["k"] = {"j"}
-maps.n["l"] = {"k"}
-maps.n[";"] = {"l"}
-maps.n["'"] = {";"}
-maps.n["\\"] = {"'"}
+maps.n["k"] = {"gj"}
+maps.n["l"] = {"gk"}
+maps.n["ş"] = {"l"}
+maps.n["i"] = {";"}
+maps.n[","] = {"'"}
 maps.n["h"] = {"\\"}
 
 maps.n["J"] = {"H"}
 maps.n["K"] = {"J"}
 maps.n["L"] = {"K"}
-maps.n[":"] = {"L"}
-maps.n["\""] = {":"}
-maps.n["|"] = {"\""}
+maps.n["Ş"] = {"L"}
+maps.n["İ"] = {":"}
+maps.n[";"] = {"\""}
 maps.n["H"] = {"|"}
--- [[~QWERTYUIOP{}ASDFGHJKL:"|ZXCVBNM<>?`qwertyuiop[]asdfghjkl;'\zxcvbnm,./]]
--- [[éQWERTYUIOPĞÜASDFGHJKLŞİ;ZXCVBNMÖÇ:"qwertyuıopğüasdfghjklşi,zxcvbnmöç.]]
+
+-- maps.v["j"] = {"h"}
+-- maps.v["k"] = {"gj"}
+-- maps.v["l"] = {"gk"}
+-- maps.v[";"] = {"l"}
+-- maps.v["'"] = {";"}
+-- maps.v["\\"] = {"'"}
+-- maps.v["h"] = {"\\"}
+--
+-- maps.v["J"] = {"H"}
+-- maps.v["K"] = {"J"}
+-- maps.v["L"] = {"K"}
+-- maps.v[":"] = {"L"}
+-- maps.v["\""] = {":"}
+-- maps.v["|"] = {"\""}
+-- maps.v["H"] = {"|"}
+
+maps.v["j"] = {"h"}
+maps.v["k"] = {"gj"}
+maps.v["l"] = {"gk"}
+maps.v["ş"] = {"l"}
+maps.v["i"] = {";"}
+maps.v[","] = {"'"}
+maps.v["h"] = {"\\"}
+
+maps.v["J"] = {"H"}
+maps.v["K"] = {"J"}
+maps.v["L"] = {"K"}
+maps.v["Ş"] = {"L"}
+maps.v["İ"] = {":"}
+maps.v[";"] = {"\""}
+maps.v["H"] = {"|"}
+
+-- maps.o["j"] = {"h"}
+-- maps.o["k"] = {"gj"}
+-- maps.o["l"] = {"gk"}
+-- maps.o[";"] = {"l"}
+-- maps.o["'"] = {";"}
+-- maps.o["\\"] = {"'"}
+-- maps.o["h"] = {"\\"}
+--
+-- maps.o["J"] = {"H"}
+-- maps.o["K"] = {"J"}
+-- maps.o["L"] = {"K"}
+-- maps.o[":"] = {"L"}
+-- maps.o["\""] = {":"}
+-- maps.o["|"] = {"\""}
+-- maps.o["H"] = {"|"}
+
+maps.o["j"] = {"h"}
+maps.o["k"] = {"gj"}
+maps.o["l"] = {"gk"}
+maps.o["ş"] = {"l"}
+maps.o["i"] = {";"}
+maps.o[","] = {"'"}
+maps.o["h"] = {"\\"}
+
+maps.o["J"] = {"H"}
+maps.o["K"] = {"J"}
+maps.o["L"] = {"K"}
+maps.o["Ş"] = {"L"}
+maps.o["İ"] = {":"}
+maps.o[";"] = {"\""}
+maps.o["H"] = {"|"}
+
+maps.n["H"] = { "<cmd>vsplit<cr>", desc = "Vertical Split" }
+maps.n["h"] = { "<cmd>split<cr>", desc = "Horizontal Split" }
+
+-- maps.n["\\"] = { "<cmd>lua require(\"which-key\").show(\"'\", {mode = \"n\", auto = true})<cr>" }
+-- maps.n["|"] = { "<cmd>lua require(\"which-key\").show(\"\\\"\", {mode = \"n\", auto = true})<cr>" }
+-- maps.n["|"] = { "<cmd>lua require(\"which-key\").show(\"\\\"\", {mode = \"v\", auto = true})<cr>" }
+
+maps.n[","] = { "<cmd>lua require(\"which-key\").show(\"'\", {mode = \"n\", auto = true})<cr>" }
+maps.n[";"] = { "<cmd>lua require(\"which-key\").show(\"\\\"\", {mode = \"n\", auto = true})<cr>" }
+maps.n[";"] = { "<cmd>lua require(\"which-key\").show(\"\\\"\", {mode = \"v\", auto = true})<cr>" }
+
+-- if vim.g.neovide then
+--     function toggle_nvde_transparancy()
+--         if vim.g.neovide_transparency == 1.0 then
+--             vim.g.neovide_transparency = 0.8
+--         else
+--             vim.g.neovide_transparency = 1.0
+--         end
+--     end
+-- end
+--
+-- maps.n["<leader>x"] = {toggle_nvde_transparancy(), desc = "Neovide transparency toggle" }
+
+if vim.g.neovide then
+--     maps.n["<leader>xi"] = {"<cmd>lua vim.g.neovide_transparency = vim.g.neovide_transparency - 0.2<cr>", desc = "Increase Neovide transparency" }
+--     maps.n["<leader>xd"] = {"<cmd>lua vim.g.neovide_transparency = vim.g.neovide_transparency + 0.2<cr>", desc = "Decrease Neovide transparency" }
+    maps.n["<C-Up>"] = {"<cmd>lua vim.g.neovide_transparency = vim.g.neovide_transparency - 0.1<cr>", desc = "Increase Neovide transparency" }
+    maps.n["<C-Down>"] = {"<cmd>lua vim.g.neovide_transparency = vim.g.neovide_transparency + 0.1<cr>", desc = "Decrease Neovide transparency" }
+end
+
+
+
+
 utils.set_mappings(maps)
 return M
--- lua/plugins/5-z-lang-mapper.lua
+++ lua/plugins/5-z-lang-mapper.lua
@@ -1,62 +1,58 @@
-local function escape(str)
-  -- You need to escape these characters to work correctly
-  local escape_chars = [[;,."|\]]
-  return vim.fn.escape(str, escape_chars)
-end
-
--- Recommended to use lua template string
-local en = [[`qwertyuiop[]asdfghjkl;'\zxcvbnm,./]]
-local tr = [["qwertyuıopğüasdfghjklşi,zxcvbnmöç.]] -- vi layout
--- local tr = [["qwertyuıopğüasdfgjklşi,hzxcvbnmöç.]]
-
-local en_shift = [[~QWERTYUIOP{}ASDFGHJKL:"|ZXCVBNM<>?]]
-local tr_shift = [[éQWERTYUIOPĞÜASDFGHJKLŞİ;ZXCVBNMÖÇ:]] -- vi layout
--- local tr_shift = [[éQWERTYUIOPĞÜASDFGJKLŞİ;HZXCVBNMÖÇ:]]
-
-vim.opt.langmap = vim.fn.join({
-    -- | `to` should be first     | `from` should be second
-    escape(tr_shift) .. ';' .. escape(en_shift),
-    escape(tr) .. ';' .. escape(en),
-}, ',')
-
-local default_config = {
-  ---@type boolean Add mapping for every CTRL+ binding or not.
-  map_all_ctrl = true,
-  ---@type string[] Modes to `map_all_ctrl`
-  ---Here and below each mode must be specified, even if some of them extend others.
-  ---E.g., 'v' includes 'x' and 's', but must be listed separate.
-  ctrl_map_modes = { 'n', 'o', 'i', 'c', 't', 'v' },
-  ---@type boolean Wrap all keymap's functions (nvim_set_keymap etc)
-  hack_keymap = true,
-  ---@type string[] Usually you don't want insert mode commands to be translated when hacking.
-  ---This does not affect normal wrapper functions, such as `langmapper.map`
-  disable_hack_modes = { 'i' },
-  ---@type table Modes whose mappings will be checked during automapping.
-  automapping_modes = { 'n', 'v', 'x', 's' },
-  ---@type string Standart English layout (on Mac, It may be different in your case.)
-  default_layout = [[~QWERTYUIOP{}ASDFGHJKL:"|ZXCVBNM<>?`qwertyuiop[]asdfghjkl;'\zxcvbnm,./]],
-  ---@type string[] Names of layouts. If empty, will handle all configured layouts.
-  use_layouts = {"tr"},
-  ---@type table Fallback layouts
-  layouts = {
-    ---@type table Fallback layout item. Name of key is a name of language
-    tr = {
-      ---@type string Name of your second keyboard layout in system.
-      ---It should be the same as result string of `get_current_layout_id()`
-      id = 'tr',
-      ---@type string Fallback layout to translate. Should be same length as default layout
-      layout = [[éQWERTYUIOPĞÜASDFGHJKLŞİ;ZXCVBNMÖÇ:"qwertyuıopğüasdfghjklşi,zxcvbnmöç.]],  -- vi layout
-
---       layout = [[éQWERTYUIOPĞÜASDFGJKLŞİ;HZXCVBNMÖÇ:"qwertyuıopğüasdfgjklşi,hzxcvbnmöç.]],
-      ---@type string if you need to specify default layout for this fallback layout
-    },
-  },
-}
+-- local function escape(str)
+--   -- You need to escape these characters to work correctly
+--   local escape_chars = [[;,."|\]]
+--   return vim.fn.escape(str, escape_chars)
+-- end
+--
+-- -- Recommended to use lua template string
+-- local en = [[`qwertyuiop[]asdfghjkl;'\zxcvbnm,./]]
+-- local tr = [["qwertyuıopğüasdfghjklşi,zxcvbnmöç.]]
+--
+-- local en_shift = [[~QWERTYUIOP{}ASDFGHJKL:"|ZXCVBNM<>?]]
+-- local tr_shift = [[éQWERTYUIOPĞÜASDFGHJKLŞİ;ZXCVBNMÖÇ:]]
+--
+-- vim.opt.langmap = vim.fn.join({
+--     -- | `to` should be first     | `from` should be second
+--     escape(tr_shift) .. ';' .. escape(en_shift),
+--     escape(tr) .. ';' .. escape(en),
+-- }, ',')
+--
+-- local default_config = {
+--   ---@type boolean Add mapping for every CTRL+ binding or not.
+--   map_all_ctrl = true,
+--   ---@type string[] Modes to `map_all_ctrl`
+--   ---Here and below each mode must be specified, even if some of them extend others.
+--   ---E.g., 'v' includes 'x' and 's', but must be listed separate.
+--   ctrl_map_modes = { 'n', 'o', 'i', 'c', 't', 'v' },
+--   ---@type boolean Wrap all keymap's functions (nvim_set_keymap etc)
+--   hack_keymap = true,
+--   ---@type string[] Usually you don't want insert mode commands to be translated when hacking.
+--   ---This does not affect normal wrapper functions, such as `langmapper.map`
+--   disable_hack_modes = { 'i' },
+--   ---@type table Modes whose mappings will be checked during automapping.
+--   automapping_modes = { 'n', 'v', 'x', 's' },
+--   ---@type string Standart English layout (on Mac, It may be different in your case.)
+--   default_layout = [[~QWERTYUIOP{}ASDFGHJKL:"|ZXCVBNM<>?`qwertyuiop[]asdfghjkl;'\zxcvbnm,./]],
+--   ---@type string[] Names of layouts. If empty, will handle all configured layouts.
+--   use_layouts = {"tr"},
+--   ---@type table Fallback layouts
+--   layouts = {
+--     ---@type table Fallback layout item. Name of key is a name of language
+--     tr = {
+--       ---@type string Name of your second keyboard layout in system.
+--       ---It should be the same as result string of `get_current_layout_id()`
+--       id = 'tr',
+--       ---@type string Fallback layout to translate. Should be same length as default layout
+--       layout = [[éQWERTYUIOPĞÜASDFGHJKLŞİ;ZXCVBNMÖÇ:"qwertyuıopğüasdfghjklşi,zxcvbnmöç.]],
+--       ---@type string if you need to specify default layout for this fallback layout
+--     },
+--   },
+-- }
 return {
-  'Wansmer/langmapper.nvim',
-  lazy = false,
-  priority = 1, -- High priority is needed if you will use `autoremap()`
-  config = function()
-    require('langmapper').setup(default_config)
-  end,
+--   'Wansmer/langmapper.nvim',
+--   lazy = false,
+--   priority = 1, -- High priority is needed if you will use `autoremap()`
+--   config = function()
+--     require('langmapper').setup(default_config)
+--   end,
 }
